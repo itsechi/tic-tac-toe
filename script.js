9f6d8f0b8628f0da9c55f5313d18ed071beebd18
itsechi <wardulinskao@gmail.com>
2022-08-04 15:56:00 +0200

    Add a function to generate gameboard

---
+++ script.js
@@ -0,0 +1,20 @@
+const gameboardContainer = document.getElementById('gameboard');
+
+const Gameboard = (() => {
+  // create the gameboard
+  (() => {
+    for (let i = 1; i <= 9; i++) {
+      const cell = document.createElement('div');
+      cell.setAttribute('class', 'cell');
+      cell.setAttribute('data-cell', `${i}`);
+      gameboardContainer.appendChild(cell);
+    }
+  })();
+  // click the cells
+  gameboardContainer.addEventListener('click', (e) => {
+    const markCell = e.target;
+    console.log(markCell);
+    markCell.classList.add('x');
+  })
+})();
+
