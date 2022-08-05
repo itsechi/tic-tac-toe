9b1c6cee6276d6548d25f644ea89c182c23a3dbe
itsechi <wardulinskao@gmail.com>
2022-08-05 22:25:11 +0200

    Add a function to restart the game

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const gameboardContainer = document.getElementById('gameboard');
 const Gameboard = (() => {
   const gameboardArr = [];
 
-  (() => {
+  const createBoard = () => {
     for (let i = 1; i <= 9; i++) {
       const cell = document.createElement('div');
       cell.setAttribute('class', 'cell');
@@ -13,9 +13,9 @@ const Gameboard = (() => {
       gameboardContainer.appendChild(cell);
       gameboardArr.push(cell);
     }
-  })();
-
-  return { gameboardArr };
+  };
+  createBoard();
+  return { gameboardArr, createBoard };
 })();
 
 // player factory
@@ -30,9 +30,15 @@ const Player = sign => {
 };
 
 const Game = (() => {
-  // create players
   const playerX = Player('X');
   const playerO = Player('O');
+  const overlay = document.getElementById('overlay');
+  const endGameModal = document.getElementById('endGameModal');
+  const endGameMessage = document.getElementById('endGameMessage');
+  const endGameBtn = document.getElementById('endGameBtn');
+  const slider = document.querySelector('.slider');
+  const playerXActiveSlider = document.querySelector('.player-X');
+  const playerOActiveSlider = document.querySelector('.player-O');
 
   // function to switch turns
   function switchTurns(player) {
@@ -42,10 +48,6 @@ const Game = (() => {
 
   // make the slider move and change colors
   function changeActiveSlider() {
-    const slider = document.querySelector('.slider');
-    const playerXActiveSlider = document.querySelector('.player-X');
-    const playerOActiveSlider = document.querySelector('.player-O');
-    
     slider.classList.toggle('O');
     playerXActiveSlider.classList.toggle('active');
     playerOActiveSlider.classList.toggle('active');
@@ -66,30 +68,53 @@ const Game = (() => {
         switchTurns(playerX);
         switchTurns(playerO);
         changeActiveSlider();
-
+        checkWin('X');
       } else if (playerO.activePlayer) {
         playerO.mark(markCell);
         switchTurns(playerO);
         switchTurns(playerX);
         changeActiveSlider();
+        checkWin('O');
       }
-      console.log(markCell);
     }
   });
+
+  function checkWin(sign) {
+    const winningCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    const won = winningCombinations.some(combination => {
+      return combination.every(i => {
+        return Gameboard.gameboardArr[i].classList.contains(sign);
+      });
+    });
+    if (won) {
+      overlay.classList.toggle('hidden');
+      endGameModal.classList.toggle('hidden');
+      endGameMessage.textContent = `Player ${sign} has won!`;
+    }
+  }
+
+  endGameBtn.addEventListener('click', restartGame);
+  function restartGame() {
+    Gameboard.gameboardArr.forEach(cell => {
+      cell.classList.remove('X');
+      cell.classList.remove('O');
+      cell.textContent = '';
+    });
+    overlay.classList.toggle('hidden');
+    endGameModal.classList.toggle('hidden');
+    slider.classList.remove('O');
+    playerXActiveSlider.classList.add('active');
+    playerOActiveSlider.classList.remove('active');
+    playerX.activePlayer = true;
+    playerO.activePlayer = false;
+  }
 })();
-
-
-
-
-
-
-// slider.classList.add('O');
-// playerXActiveSlider.classList.remove('active');
-// playerOActiveSlider.classList.add('active');
-
-
-// slider.classList.remove('O');
-// playerXActiveSlider.classList.add('active');
-// playerOActiveSlider.classList.remove('active');
-
-
